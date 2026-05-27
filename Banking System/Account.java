f9f9890c44fbb4faa64cdf9c04c204504cba8441
Subair Osman <subairosman6@gmail.com>
2026-05-27 18:47:00 +0100

    Initial banking System setup

---
+++ Banking System/Account.java
@@ -0,0 +1,40 @@
+class Account{
+
+   private String name;
+   private double balance; 
+
+   public Account(String name, double balance) {
+
+     this.name = name;
+     this.balance = balance;
+
+   }
+
+   public void deposite(double amount) {
+     if(amount < 0) {
+      System.out.println("Invalid for deposite of N");
+     }
+     
+     else {balance += amount;
+     }
+   }
+
+   public double displayBalance() {
+     return balance;
+
+   }
+
+   public void withdraw(double amount) {
+     if(amount < 0) {
+      System.out.println("Invalid for withdraws of N");
+     }
+
+     else if(balance < amount) {
+      System.out.println("insufficent funds");
+     }
+     else {balance -= amount;
+     }
+   }
+}
+    
+
---
+++ Banking System/Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+    public static void main(String[] args) {
+        Account su = new Account("Subair", 200.00);
+        su.deposite(-1);
+        System.out.println(su.displayBalance());
+        su.withdraw(-20);
+        System.out.println(su.displayBalance());
+
+    }
+}
